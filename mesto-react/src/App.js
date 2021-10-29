bc8a1b5ce7e23e6b09b84ebb68190d65caf9f85b
kiraanikeev <kiraanikev@mail.ru>
2021-10-29 15:27:52 +0300

    create project

---
+++ mesto-react/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <p>mesto-react</p>
+
+    </div>
+  );
+}
+
+export default App;
